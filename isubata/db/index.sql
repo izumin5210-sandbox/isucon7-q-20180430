e2ad34f7f8e3ca5ca4ad77268cdcd812148fe961
tako <rina.m.takase@gmail.com>
2018-04-30 19:56:25 +0900

    semicolon

--- isubata/db/index.sql
+++ isubata/db/index.sql
@@ -1,3 +1,3 @@
-CREATE INDEX index_user_on_name ON user(name)
-CREATE INDEX index_message_on_id_channel_id ON message(id, channel_id)
-CREATE INDEX index_message_on_channel_id ON message(channel_id)
+CREATE INDEX index_user_on_name ON user(name);
+CREATE INDEX index_message_on_id_channel_id ON message(id, channel_id);
+CREATE INDEX index_message_on_channel_id ON message(channel_id);
